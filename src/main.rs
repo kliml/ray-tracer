c3efffad614b83d389c94b49d7d8e29f4ca4c7a2
Klim <klim.lestsenko@gmail.com>
2020-08-22 21:43:18 +0300

    renderer init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // Image
+
+    let image_width = 256;
+    let image_height = 256;
+
+    // Renderer
+
+    print!("P3\n{} {}\n255\n", image_width, image_height);
+
+    for j in (0..image_height).rev() {
+        eprint!("\rScanlines remaining: {}", j);
+        for i in 0..image_width {
+            let r = i as f32 / (image_width - 1) as f32;
+            let g = j as f32 / (image_height - 1) as f32;
+            let b = 0.25;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+
+            print!("{} {} {}\n", ir, ig, ib);
+        }
+    }
+
+    eprint!("\nDone.\n");
+}
